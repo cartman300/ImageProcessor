076fe915c2a3671c598386126d303a645526110b
Saša Barišić <cartman300@net.hr>
2016-08-06 04:11:13 +0200

    Added console app for tests

---
+++ tests/ImageProcessorCore.DevTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ImageProcessorCore;
+using ImageProcessorCore.Formats;
+using ImageProcessorCore.Processors;
+using ImageProcessorCore.Quantizers;
+
+namespace ImageProcessorCore.DevTest {
+	public class Program {
+		public static void Main(string[] args) {
+			Run();
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+
+		static void Run() {
+
+		}
+	}
+}
---
+++ tests/ImageProcessorCore.DevTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageProcessorCore.DevTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3eb4b10-f89d-4fd5-8b06-4892a9f64a98")]
